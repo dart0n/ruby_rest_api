c848963e76c636d126a5fd8b3b46adb7bf699dc7
dart0n <daria.tonkonoh@gmail.com>
2021-04-11 16:48:02 +0300

    Add repository methods

--- users/users_repository.rb
+++ users/users_repository.rb
@@ -4,12 +4,33 @@ class UsersRepostory
   @table = 'users'
 
   def self.create(firstname, lastname, year_salary)
-    Database.execute(
-      "insert into #{@table} (firstname, lastname, year_salary) values ('#{firstname}', '#{lastname}', #{year_salary})"
+    Database.execute_with_result(
+      "insert into #{@table} (firstname, lastname, year_salary)" \
+      "values ('#{firstname}', '#{lastname}', #{year_salary}) returning *"
     )
   end
 
-  def self.users
+  def self.all
     Database.execute_with_result("select * from #{@table}")
+  end
+
+  def self.find_by_id(id)
+    Database.execute_with_result("select * from #{@table} where id = #{id}")
+  end
+
+  def self.update(id, firstname, lastname, year_salary)
+    Database.execute_with_result(
+      "update #{@table} " \
+      "set firstname = '#{firstname}', lastname = '#{lastname}', year_salary = #{year_salary} " \
+      "where id = #{id} returning *"
+    )
+  end
+
+  def self.destroy(id)
+    Database.execute("delete from #{@table} where id = #{id}")
+  end
+
+  def self.order(field, order_type)
+    Database.execute_with_result("select * from #{@table} order by #{field} #{order_type}")
   end
 end
