ad819f8c874d2adeff07c83144363d58b630c68d
dart0n <daria.tonkonoh@gmail.com>
2021-04-11 16:45:33 +0300

    Add controller methods

--- users/users_controller.rb
+++ users/users_controller.rb
@@ -2,10 +2,22 @@ require_relative './users_service'
 
 class UsersController
   def self.create(req)
-    UsersService.create(req[:body])
+    UsersService.create(req)
   end
 
   def self.index
-    UsersService.users
+    UsersService.all
+  end
+
+  def self.update(id, req)
+    UsersService.update(id, req)
+  end
+
+  def self.destroy(id)
+    UsersService.destroy(id)
+  end
+
+  def self.sort(sort_option)
+    UsersService.order(sort_option)
   end
 end
